66efafd235c188a61acdce9334e994f5df6ef1e2
shuvava <shuvava@users.noreply.github.com>
2024-07-16 19:51:18 -0700

    upd repo url

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     description="Streamlit component that allows you catch arrow up keyboard event",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="",
+    url="https://github.com/shuvava/streamlit-chat-input",
     packages=setuptools.find_packages(),
     include_package_data=True,
     classifiers=[],
